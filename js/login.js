241e02c4708b33069b7dca57014222c2746ff87e
ivan-burgio <ivan.24.burgio@gmail.com>
2023-08-12 12:17:26 -0300

    Funciones realacionadas a las alertas y parte logica del login terminadas

--- js/login.js
+++ js/login.js
@@ -1,34 +1,41 @@
-function mostrarAlerta(mensaje, tipo, referencia) {
-    // Previene la creacion de multiples alertas
-    const alertaPrevia = document.querySelector('.alerta');
-
-    if(alertaPrevia) {
-        alertaPrevia.remove();
-    }
-
+function crearAlerta(mensaje, tipo, referencia) {
     const alerta = document.createElement('DIV');
     alerta.classList.add('alerta', tipo);
     alerta.textContent = mensaje;
 
-    // Insertar la alerta despues del legend
+    // Insertar la alerta despues del h1
     referencia.parentElement.insertBefore(alerta, referencia.nextElementSibling);
 
     // Eliminar la alerta
     setTimeout(() => {
         alerta.remove();
-    }, 5000);
+    }, 4000);
 }
 
-mostrarAlerta(resultado.mensaje, resultado.tipo, document.querySelector('.container-login h1'));
+function mostrarAlertas() {
+    const mail = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value;
+
+    if(mail.length === 0) {
+        alertas = crearAlerta('Ingrese un correo', 'error', document.querySelector('.container-login h1'));
+    }
+    if(password.length === 0) {
+        alertas = crearAlerta('Ingrese una contraseña', 'error', document.querySelector('.container-login h1'));
+    }
+
+    return alertas;
+}
 
 // Espacio de comprobaciones
-const registrarBtn = document.querySelector('.button-login');
+const loginBtn = document.querySelector('.button-login');
 
-registrarBtn.addEventListener('click', () => {
-    const mail = document.querySelector('#email').value.trim();
-    const password = document.getElementById("password");
-    const confirmPassword = password.value;
+loginBtn.addEventListener('click', () => {
+    const alertas = [];
+    mostrarAlertas();
+
+    
+    if(alertas.length === 0) {
+    // Redireccionamiento si no hay alertas
+        
+    }
 });
-
-(mail.length === 0)
-(password.length === 0)
