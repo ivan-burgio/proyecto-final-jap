d0f5d9aa4a9d45cc0c5ade1bc1fa63b32c21c372
Daniel-Abreu-94 <141192601+Daniel-Abreu-94@users.noreply.github.com>
2023-08-16 20:06:25 -0300

    products-final

--- js/products.js
+++ js/products.js
@@ -1,32 +1,34 @@
-
-getJSONData("https://japceibal.github.io/emercado-api/products/101.json")
+fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
+  .then(response => response.json())
   .then(result => {
-    if (result.status === 'ok') {
+    if (result.catID === 101) { // Verificar el catID si es necesario
       const productContainer = document.getElementById("product-list-container"); 
       
-
-      result.data.forEach(producto => {
-        const productCard = ` <div class"row list-group-item d-flex justify-content-start">
-          <div class="col-3">
-            <img src="${producto.imgSrc}" alt="${producto.name}">
+      result.products.forEach(producto => {
+        const productCard = `
+          <div class="row list-group-item d-flex justify-content-start">
+            <div class="col-3">
+              <img src="${producto.image}" alt="${producto.name}" style="max-width: 100%; height: auto;">
             </div>
             <div class="col-7">
-            <h3>${producto.name}</h3>
-            <p>${producto.description}</p>
+              <h3>${producto.name} - USD ${producto.cost}</h3>
+              <p>${producto.description}</p>
+              </div>
+              <div class="col-2 text-muted text-end" >
+              <small>${producto.soldCount} vendidos</small>
             </div>
-            <div class="col-2 text muted">
-            <small>Product Count: ${producto.price}</small>
             </div>
+            
           </div>
         `;
         productContainer.innerHTML += productCard;
-      });
+      });
 
-      console.log('Datos obtenidos:', result.data);
+      console.log('Datos obtenidos:', result);
     } else {
-      console.error('Ocurrió un error:', result.data);
+      console.error('No se encontraron productos para la categoría especificada.');
     }
   })
   .catch(error => {
-    console.error('Error en la solicitud:', error);
-  });
+    console.error('Error en la solicitud:', error);
+  });
