b8a50ec15572d3f871e69b7417416e0fd714596b
Daniel-Abreu-94 <141192601+Daniel-Abreu-94@users.noreply.github.com>
2023-10-26 01:09:09 -0300

    avances

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,11 @@ const containerCart = document.getElementById("container-cart");
 const buttonLeft = document.getElementsByClassName("number-left");
 const buttonRight = document.getElementsByClassName("number-right");
 const valueCountArticle = document.getElementsByName("number-count");
+const cardPriceContainer= document.getElementById("cardTotalPrice");
 let count = 1;
+let subTotal = 0;
+let costoDeEnvio = 0;
+let Total = 0;
 
 
 //Obtiene los datos del fetch del carrito y los guarda en un array
@@ -19,12 +23,16 @@ function getDataCartUser() {
     .catch(error => {
       console.error('Error en la solicitud:', error);
     });
+    
 }
 
 //Muestra el contenido de un array
 function showCartList(array) {
     let cartUser = ``;
     for (i= 0; i < array.length; i++) {
+      subTotal  += arrayItemCart[i].unitCost;
+      Total = subTotal + costoDeEnvio;
+    
         cartUser = `
             <div class="card-product shadow align-items-center caja-gris-raro">
                 <div class="card-1 container-fluid">
@@ -55,27 +63,52 @@ function showCartList(array) {
         `
         containerCart.innerHTML += cartUser;
     }
+    //tarjeta de precios
+    let cardPrice = `<div><h3>Costos</h3></div> <br>
+    <div><h4>Subtotal: USD ${subTotal}</h4></div> <br>
+    <div id="envio"><h4>Costo de envio: USD ${costoDeEnvio}</h4></div> <br>
+    <div id="total"><h4>Total: USD ${Total} </h4></div>
+    `
+    
+        
+              containerCart.innerHTML += cardPrice; 
 }
-document.addEventListener("DOMContentLoaded", getDataCartUser);
+
 
 function sumIndividualCost(i, unitCost, count, currency){
 
 input = document.getElementById(i + "_modified");
  input.value++
+ subTotal += unitCost
+ Total += unitCost
  const subtotalCost = input.value*unitCost;
  subtotal = document.getElementById(i + "_Subtotal");
  subtotal.textContent = `Subtotal:  ${currency.toString()} ${subtotalCost}`
  const cartActual = localStorage.getItem('cartItem');
  JSON.parse(cartActual);
+ 
+ Total = subTotal + costoDeEnvio;
+    const costoDeEnvioElement = document.getElementById('envio');
+    const totalElement = document.getElementById('total');
+    costoDeEnvioElement.textContent = `Costo de envio: USD ${costoDeEnvio}`;
+    totalElement.textContent = `Total: USD ${Total}`;
 }
 
 function restIndividualCost(i, unitCost, count, currency){
 
     input = document.getElementById(i + "_modified");
      if(input.value>0){input.value--}
+     subTotal -= unitCost
+     Total -= unitCost
      const subtotalCost = input.value*unitCost;
      subtotal = document.getElementById(i + "_Subtotal");
-     subtotal.textContent = `Subtotal:  ${currency.toString()} ${subtotalCost}` 
+     subtotal.textContent = `Subtotal:  ${currency.toString()} ${subtotalCost}`
+     
+     Total = subTotal + costoDeEnvio;
+    const costoDeEnvioElement = document.getElementById('envio');
+    const totalElement = document.getElementById('total');
+    costoDeEnvioElement.textContent = `Costo de envio: USD ${costoDeEnvio}`;
+    totalElement.textContent = `Total: USD ${Total}`;
     }
 
     function deleteItem(Item){
@@ -92,7 +125,6 @@ function restIndividualCost(i, unitCost, count, currency){
      showCartList(setLocalProduct)
     }
 
-
 const botonMostrarFormulario = document.getElementById("mostrarFormulario");
 const formulario = document.getElementById("formulario");
 
@@ -103,3 +135,29 @@ botonMostrarFormulario.addEventListener("click", function() {
       formulario.style.display = "none";
     }
   });
+// calculadora de envios
+  function envioStandard(){
+    costoDeEnvio = subTotal * 0.05;
+    updateTotal();
+}
+
+function envioExpress(){
+    costoDeEnvio = subTotal * 0.07;
+    updateTotal();
+}
+
+function envioPremium(){
+    costoDeEnvio = subTotal * 0.15;
+    updateTotal();
+}
+
+  function updateTotal() {
+    Total = subTotal + costoDeEnvio;
+    const costoDeEnvioElement = document.getElementById('envio');
+    const totalElement = document.getElementById('total');
+    costoDeEnvioElement.textContent = `Costo de envio: USD ${costoDeEnvio}`;
+    totalElement.textContent = `Total: USD ${Total}`;
+}
+
+
+  document.addEventListener("DOMContentLoaded", getDataCartUser);
